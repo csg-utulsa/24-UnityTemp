258ff2c7727eccc521bd5e78d79da58c09f3c4c5
Leslie Graff <leslienoragraff@hotmail.com>
2024-03-08 15:03:17 -0600

    camera is working

--- Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+
     static public GameObject POI;//static point of interest
-    [Header("SetDynamically")]
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+    [Header("Set Dynamically")]
     public float camZ; // the desired z poze
 
     void Awake()
@@ -19,10 +23,17 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return; //return if there is no pool
         //get the position of the poi
         Vector3 destination = POI.transform.position;
+        //limit the x&Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //Interpolate from the current Camea position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         //force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         //set the camera to the destination
         transform.position = destination;
+        //set the orthographic size of the camera to keep ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
 
